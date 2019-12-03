c3908ddcbd95af0abf7fa0c4e6d172c861e4a1ce
Andi Péter <andipeti2000@gmail.com>
2019-12-03 20:12:46 +0100

    Cleaned up day3

--- 2019/day-3/day3/src/main.rs
+++ 2019/day-3/day3/src/main.rs
@@ -2,8 +2,6 @@ use recap::Recap;
 use serde::Deserialize;
 use std::collections::HashMap;
 
-use std::cmp::{min, max};
-
 #[derive(Debug, Deserialize, Recap)]
 #[recap(regex = r#"(?P<dir>[A-Z])(?P<dist>\d+)"#)]
 struct InputInstruction {
@@ -11,130 +9,54 @@ struct InputInstruction {
     dist: i32,
 }
 
-#[derive(Debug)]
-enum Line {
-    Horizontal{y: i32, x1: i32, x2: i32},
-    Vertical  {x: i32, y1: i32, y2: i32},
-}
-
-fn add_line(prev: &(i32, i32), x: InputInstruction) -> (i32, i32) {
-    // println!("{:?}, {:?}", prev, x);
-    match x.dir {
-        'U' => (prev.0, prev.1 + x.dist),
-        'D' => (prev.0, prev.1 - x.dist),
-        'R' => (prev.0 + x.dist, prev.1),
-        'L' => (prev.0 - x.dist, prev.1),
-        _ => (20000, 20000),
+fn move_once(dir: &char, prev: &(i32, i32)) -> (i32, i32) {
+    let (x, y) = *prev;
+    match dir {
+        'U' => (x, y + 1),
+        'D' => (x, y - 1),
+        'R' => (x + 1, y),
+        'L' => (x - 1, y),
+        _ => (200000, 200000),
     }
 }
 
-fn draw_line(line: &str) -> Vec<(i32, i32)> {
-    line
-        .split(',')
-        .map(|s| s.parse::<InputInstruction>().unwrap())
-        .fold(vec![(0,0)], |mut acc, x| {
-            acc.push(add_line(acc.last().unwrap(), x));
-            acc
-        })
-}
-
-fn to_line(x: &(i32, i32), y: &(i32, i32)) -> Line {
-    if x.0 == y.0 {
-        Line::Vertical{
-            x: x.0,
-            y1: x.1,
-            y2: y.1
-        }
-    } else {
-        Line::Horizontal{
-            y: x.1,
-            x1: x.0,
-            x2: y.0
-        }
-    }
-}
-
-fn manhattan(x: &i32, y: &i32) -> i32 {
-    x.abs() + y.abs()
+fn manhattan(x: &(i32, i32)) -> i32 {
+    x.0.abs() + x.1.abs()
 }
 
 fn main() {
     let contents = std::fs::read_to_string("input.txt").expect("Error opening file!");
     let mut lines = contents.lines();
-    let line1_raw = lines.next().unwrap();
-    let line2_raw = lines.next().unwrap();
-
-    let line1_points = draw_line(line1_raw);
-    let line2_points = draw_line(line2_raw);
-
-    let mut line1_line = Vec::new();
-    for i in 0..(line1_points.len() - 1) {
-        line1_line.push(to_line(&line1_points[i], &line1_points[i+1]));
-    }
-
-    let mut line2_line = Vec::new();
-    for i in 0..(line2_points.len() - 1) {
-        line2_line.push(to_line(&line2_points[i], &line2_points[i+1]));
-    }
+    let line1_raw: Vec<InputInstruction> = lines.next().unwrap().split(',').map(|s| s.parse().unwrap()).collect();
+    let line2_raw: Vec<InputInstruction> = lines.next().unwrap().split(',').map(|s| s.parse().unwrap()).collect();
 
     let mut line1 = HashMap::new();
-    let mut steps = 0;
-    for line in line1_line {
-        match line {
-            Line::Horizontal{y,x1,x2} => {
-                let range: Vec<i32> = if x1 <= x2 { (x1..=x2).collect() } else { (x2..=x1).rev().collect() }; 
-                for x in range {
-                    if !line1.contains_key(&(x,y)) {
-                        line1.insert((x,y), steps);
-                    }
-                    steps += 1;
-                }
-            },
-            Line::Vertical{x,y1,y2} => {
-                let range: Vec<i32> = if y1 <= y2 { (y1..=y2).collect() } else { (y2..=y1).rev().collect() }; 
-                for y in range {
-                    if !line1.contains_key(&(x,y)) {
-                        line1.insert((x,y), steps);
-                    }
-                    steps += 1;
-                }
+    let mut steps = 1;
+    let mut prev = (0,0);
+    for line in line1_raw {
+        for _ in 0..line.dist {
+            prev = move_once(&line.dir, &prev);
+            if !line1.contains_key(&prev) {
+                line1.insert(prev, steps);
             }
+            steps += 1;
         }
-        steps -= 1;
     }
 
-    // println!("{:?}", line1);
-
-    line1.remove(&(0,0));
-
+    // Part 1
     let mut found = false;
     let mut closest = 0;
-    for line in &line2_line {
-        match *line {
-            Line::Horizontal{y,x1,x2} => {
-                for x in min(x1, x2)..=max(x1,x2) {
-                    if line1.contains_key(&(x,y)) {
-                        let dist = manhattan(&x, &y);
-                        if !found {
-                            closest = dist;
-                            found = true;
-                        } else if found && closest > dist {
-                            closest = dist;
-                        }
-                    }
-                }
-            },
-            Line::Vertical{x,y1,y2} => {
-                for y in min(y1,y2) ..=max(y1,y2) {
-                    if line1.contains_key(&(x,y)) {
-                        let dist = manhattan(&x, &y);
-                        if !found {
-                            closest = dist;
-                            found = true;
-                        } else if found && closest > dist {
-                            closest = dist;
-                        }
-                    }
+    let mut prev = (0,0);
+    for line in &line2_raw {
+        for _ in 0..line.dist {
+            prev = move_once(&line.dir, &prev);
+            if line1.contains_key(&prev) {
+                let dist = manhattan(&prev);
+                if !found {
+                    closest = dist;
+                    found = true;
+                } else if found && closest > dist {
+                    closest = dist;
                 }
             }
         }
@@ -142,43 +64,25 @@ fn main() {
 
     println!("Part 1: {}", closest);
 
+    // Part 2
     let mut found = false;
     let mut closest = 0;
-    let mut steps = 0;
-    for line in line2_line {
-        match line {
-            Line::Horizontal{y,x1,x2} => {
-                let range: Vec<i32> = if x1 <= x2 { (x1..=x2).collect() } else { (x2..=x1).rev().collect() }; 
-                for x in range {
-                    if let Some(line1_dist) = line1.get(&(x,y)) {
-                        let dist = *line1_dist + steps;
-                        if !found {
-                            closest = dist;
-                            found = true;
-                        } else if found && closest > dist {
-                            closest = dist;
-                        }
-                    }
-                    steps += 1;
-                }
-            },
-            Line::Vertical{x,y1,y2} => {
-                let range: Vec<i32> = if y1 <= y2 { (y1..=y2).collect() } else { (y2..=y1).rev().collect() }; 
-                for y in range {
-                    if let Some(line1_dist) = line1.get(&(x,y)) {
-                        let dist = *line1_dist + steps;
-                        if !found {
-                            closest = dist;
-                            found = true;
-                        } else if found && closest > dist {
-                            closest = dist;
-                        }
-                    }
-                    steps += 1;
+    let mut steps = 1;
+    let mut prev = (0,0);
+    for line in line2_raw {
+        for _ in 0..line.dist {
+            prev = move_once(&line.dir, &prev);
+            if let Some(line1_dist) = line1.get(&prev) {
+                let dist = *line1_dist + steps;
+                if !found {
+                    closest = dist;
+                    found = true;
+                } else if found && closest > dist {
+                    closest = dist;
                 }
             }
+            steps += 1;
         }
-        steps -= 1;
     }
 
     println!("Part 2: {}", closest);
