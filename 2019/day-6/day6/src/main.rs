03af10e892e357caf02b6ab49b19a0c5a9fce457
Andi Péter <andipeti2000@gmail.com>
2019-12-07 10:50:38 +0100

    Updated day6 to recursive bfs

--- 2019/day-6/day6/src/main.rs
+++ 2019/day-6/day6/src/main.rs
@@ -1,74 +1,57 @@
 use std::collections::HashMap;
-use std::collections::BinaryHeap;
+
+fn part1_bfs<'a>(u: &'a str, graph: &HashMap<&'a str, Vec<&'a str>>, visited: &mut HashMap<&'a str, bool>, dist: u32) -> u32 {
+    if visited[u] {
+        return 0;
+    }
+
+    visited.insert(u, true);
+
+    let mut sum = dist;
+    for v in graph.get(u).unwrap() {
+        sum += part1_bfs(v, &graph, visited, dist + 1);
+    }
+
+    sum
+}
+
+fn part2_bfs<'a>(u: &'a str, graph: &HashMap<&'a str, Vec<&'a str>>, visited: &mut HashMap<&'a str, bool>, dist: u32) -> u32 {
+    visited.insert(u, true);
+
+    let mut san_dist = 0;
+    for v in graph.get(u).unwrap() {
+        if v == &"SAN" {
+            return dist;
+        }
+        if !visited[v] {
+            san_dist += part2_bfs(v, &graph, visited, dist + 1);
+        }
+    }
+
+    san_dist
+}
 
 fn main() {
     let contents = std::fs::read_to_string("input.txt")
         .expect("Error opening file!");
 
     let mut graph:   HashMap<&str, Vec<&str>> = HashMap::new();
-    let mut dist:    HashMap<&str, i32>       = HashMap::new();
-    let mut visited: HashMap<&str, bool>      = HashMap::new();
+    let mut visited: HashMap<&str, bool     > = HashMap::new();
     for edge in contents.lines() {
         let uv: Vec<&str> = edge.split(")").collect();
-        graph.entry(uv[0]).or_insert(Vec::new()).push(uv[1]);
-        visited.insert(uv[0], false);
 
+        graph.entry(uv[0]).or_insert(Vec::new()).push(uv[1]);
         graph.entry(uv[1]).or_insert(Vec::new()).push(uv[0]);
+
+        visited.insert(uv[0], false);
         visited.insert(uv[1], false);
     }
 
-    dist.insert("COM", 0);
-    visited.insert("COM", true);
-    let mut queue: BinaryHeap<&str> = BinaryHeap::new();
-    queue.push("COM");
-
-    let mut sum = 0;
-    while !queue.is_empty() {
-        let u = queue.pop().unwrap();
-        let udist: i32 = *dist.get(u).unwrap();
-
-        if let Some(vec) = graph.get(u) { 
-            for v in vec {
-                if !visited.get(v).unwrap() {
-                    sum += udist + 1;
-                    dist.insert(v, udist + 1);
-                    visited.insert(v, true);
-                    queue.push(v);
-                }
-            }
-        }
-    }
-
-    println!("Part 1: {}", sum);
+    println!("Part 1: {}", part1_bfs("COM", &graph, &mut visited, 0));
 
     for v in visited.values_mut() {
         *v = false;
     }
 
-    dist.insert("YOU", -1);
-    visited.insert("YOU", true);
-    let mut queue: BinaryHeap<&str> = BinaryHeap::new();
-    queue.push("YOU");
-
-    let mut santa_dist = 0;
-    'outer: while !queue.is_empty() {
-        let u = queue.pop().unwrap();
-        let udist: i32 = *dist.get(u).unwrap();
-
-        if let Some(vec) = graph.get(u) { 
-            for v in vec {
-                if v == &"SAN" {
-                    santa_dist = udist;
-                    break 'outer;
-                }
-                if !visited.get(v).unwrap() {
-                    dist.insert(v, udist + 1);
-                    visited.insert(v, true);
-                    queue.push(v);
-                }
-            }
-        }
-    }
-
-    println!("Part 2: {}", santa_dist);
+    println!("Part 2: {}", part2_bfs("YOU", &graph, &mut visited, 0) - 1);
 }
