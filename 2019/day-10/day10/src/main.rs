30e8f540d10116a8cbd3285c769bf6cc7d1593c2
Andi Péter <andipeti2000@gmail.com>
2019-12-10 19:01:56 +0100

    Solved day10 part 1

--- 2019/day-10/day10/src/main.rs
+++ 2019/day-10/day10/src/main.rs
@@ -1,43 +1,29 @@
 use std::collections::HashSet;
-use std::hash::{Hash, Hasher};
+use gcd::Gcd;
 
-// fn manhattan(x: &(i32, i32), y: &(i32, i32)) -> i32 {
-//     (x.0 - y.0).abs() + (x.1 - y.1).abs()
-//     // ((x.0 - y.0)*(x.0 - y.0) + (x.1 - y.1)*(x.1 - y.1)).sqrt()
-// }
+fn tan(a: &(i32, i32), b: &(i32, i32)) -> (i32, i32, i32) {
+    let u = (b.1 - a.1).abs();
+    let v = (b.0 - a.0).abs();
+    let gcd = (u as u32).gcd(v as u32) as i32;
+    let quarter = if b.0 > a.0 {
+        if b.1 > a.1 {
+            1
+        } else {
+            2
+        }
+    } else {
+        if b.1 < a.1 {
+            3
+        } else {
+            4
+        }
+    };
 
-// fn on_line(a: &(i32, i32), b: &(i32, i32), c: &(i32, i32)) -> bool {
-//     (b.1 - a.1)*(c.0 - a.0) - (c.1 - a.1)*(b.0 - a.0) == 0.0
-// }
-
-struct dist {
-    u: i32,
-    v: i32,
-}
-
-impl PartialEq for dist {
-    fn eq(&self, other: &dist) -> bool {
-        self.u as f64 / self.v as f64 ==  other.u as f64 / other.v as f64
-    }
-}
-
-impl Eq for dist {}
-
-impl Hash for dist {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        (((self.u as f64 / self.v as f64) * 100000.0) as i64).hash(state);
-    }
-}
-
-fn tan(a: &(i32, i32), b: &(i32, i32)) -> dist {
-    dist {
-        u: (b.1 - a.1).abs(), 
-        v: (b.0 - a.0).abs(),
-    }
+    (u / gcd, v / gcd, quarter)
 }
 
 fn main() {
-    let contents = std::fs::read_to_string("test.txt")
+    let contents = std::fs::read_to_string("input.txt")
         .expect("Error opening file!");
 
     let parsable: Vec<Vec<char>> = contents.lines().map(|l| l.chars().collect::<Vec<char>>()).collect();
@@ -52,41 +38,7 @@ fn main() {
         }
     }
 
-    // let mut best = 0;
-    // let mut best_pos = (0.0,0.0); 
-    // for asteroid in asteroids.clone() {
-    //     let mut count = 0;
-
-    //     for target in asteroids.clone() {
-    //         if target == asteroid {continue;}
-
-    //         let mut interferes = false;
-
-    //         for int in asteroids.clone() {
-    //             if int == asteroid || int == target {continue;}
-
-    //             if on_line(&asteroid, &target, &int) && manhattan(&asteroid, &int) < manhattan(&asteroid, &target) {
-    //                 interferes = true;
-    //                 break;
-    //             }
-    //         }
-
-    //         if !interferes {
-    //             count += 1;
-    //         }
-    //     }
-    //     if asteroid == (5.0,8.0) {
-    //         println!("{}", count);
-    //     }
-
-    //     if count > best {
-    //         best = count;
-    //         best_pos = asteroid;
-    //     }
-    // }
-
     let mut best = 0;
-    let mut best_pos = (0,0); 
     for asteroid in asteroids.clone() {
         let mut tans = HashSet::new();
         for target in asteroids.clone() {
@@ -95,15 +47,10 @@ fn main() {
             tans.insert(tan(&asteroid, &target));
         }
 
-        if asteroid == (5,8) {
-            println!("{}", tans.len());
-        }
-
         if tans.len() > best {
             best = tans.len();
-            best_pos = asteroid;
         }
     }
 
-    println!("Part 1: {} {:?}", best, best_pos);
+    println!("Part 1: {}", best);
 }
