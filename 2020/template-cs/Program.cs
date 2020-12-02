5ce29d263cc629903a8959370a740973368378bb
Petrosz007 <andipeti2000@gmail.com>
2020-12-02 08:10:11 +0100

    Added c# project template

---
+++ 2020/template-cs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.IO;
+
+namespace DayXcs
+{
+    public class Program
+    {   
+        public static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt");
+
+            var stopwatch = Stopwatch.StartNew();
+
+            var part1 = Solve1(input);
+            var part2 = Solve2(input);
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Done in {stopwatch.Elapsed.TotalMilliseconds}ms");
+            Console.WriteLine($"Part1: {part1}");
+            Console.WriteLine($"Part2: {part2}");
+        }
+    }
+}
---
+++ 2020/template-cs/Tests.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using DayXcs;
+
+namespace DayXcs.Tests
+{
+    public class Tests
+    {
+
+    }
+}
