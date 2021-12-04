f779bc8810dad22aacbbee3951dafa7507af7bd6
Petrosz007 <andipeti2000@gmail.com>
2021-12-04 10:20:53 +0000

    Clean up 2021 Day4 haskell

--- 2021/day-4/day4hs/Day4.hs
+++ 2021/day-4/day4hs/Day4.hs
@@ -1,40 +1,33 @@
 module Day4 where
 
-import Data.List (groupBy, sortBy, intercalate, find)
+import Data.List (transpose, intercalate, find)
 import Data.Text (pack, splitOn, unpack)
 import Data.Maybe (fromJust)
 import Debug.Trace (trace)
 
-data Pos = Pos
-  { row :: Int,
-    col :: Int
-  }
-  deriving (Show, Eq)
-
 data Cell = Cell
-  { pos :: Pos,
-    num :: Int,
+  { num :: Int,
     marked :: Bool
   }
   deriving (Show, Eq)
 
-type Board = [Cell]
+type Board = [[Cell]]
 
 
 {--- Utilities ---}
 split :: String -> String -> [String]
 split separator str = map unpack $ splitOn (pack separator) (pack str)
 
-update :: (a -> Bool) -> (a -> a) -> [a] -> [a]
-update pred f = map (\x -> if pred x then f x else x)
+matrixMap :: (a -> b) -> [[a]] -> [[b]]
+matrixMap f = map (map f)
+
+update :: (a -> Bool) -> (a -> a) -> [[a]] -> [[a]]
+update pred f = matrixMap (\x -> if pred x then f x else x)
 
 
 {--- Parsing ---}
 toBoard :: [[Int]] -> Board
-toBoard =
-  concatMap (\(row, xs) -> map (\(col, val) -> Cell {pos = Pos {row = row, col = col}, num = val, marked = False}) xs)
-    . map (\(row, line) -> (row, zip [0 ..] line))
-    . zip [0 ..]
+toBoard = matrixMap (\x -> Cell {num = x, marked = False})
 
 boardStringToInts :: [String] -> [[Int]]
 boardStringToInts = map (map (\x -> read x :: Int) . words)
@@ -48,21 +41,18 @@ parseInput s = (nums, boards)
     boards = map (toBoard . boardStringToInts) boardStrings
 
 
-{--- Board Manipulation ---}
+{--- Board Operations ---}
 markNumber :: Int -> Board -> Board
 markNumber target board = update ((==) target . num) (\cell -> cell {marked = True}) board
 
-cellsEqBy :: Eq a => (Cell -> a) -> Cell -> Cell -> Bool
-cellsEqBy lens x y = (lens x) == (lens y)
-
-cellsCompareBy :: Ord a => (Cell -> a) -> Cell -> Cell -> Ordering
-cellsCompareBy lens x y = compare (lens x) (lens y)
-
 rows :: Board -> [[Cell]]
-rows board = groupBy (cellsEqBy (row . pos)) $ sortBy (cellsCompareBy (row . pos)) board
+rows board = board
 
 cols :: Board -> [[Cell]]
-cols board = groupBy (cellsEqBy (col . pos)) $ sortBy (cellsCompareBy (col . pos)) board
+cols board = transpose board
+
+cells :: Board -> [Cell]
+cells = concat
 
 
 {--- Bingo Operations ---}
@@ -72,12 +62,14 @@ hasBingo board = rowBingo || colBingo
     rowBingo = or . map (and . map marked) $ rows board
     colBingo = or . map (and . map marked) $ cols board
 
-playBingoUntilFirstWin :: Int -> [Int] -> [Board] -> (Int, Board)
+type PlayBingo = Int -> [Int] -> [Board] -> (Int, Board)
+
+playBingoUntilFirstWin :: PlayBingo
 playBingoUntilFirstWin prevNumber (n:ns) boards = case find hasBingo boards of
   Just board -> (prevNumber, board)
   Nothing    -> playBingoUntilFirstWin n ns (map (markNumber n) boards)
 
-playBingoUntilLastWin :: Int -> [Int] -> [Board] -> (Int, Board)
+playBingoUntilLastWin :: PlayBingo
 playBingoUntilLastWin prevNumber (n:ns) [board] = if hasBingo board
   then (prevNumber, board)
   else playBingoUntilLastWin n ns [(markNumber n board)]
@@ -85,11 +77,11 @@ playBingoUntilLastWin prevNumber (n:ns) boards = playBingoUntilLastWin n ns . ma
 
 
 {--- Solutions ---}
-solve :: (Int -> [Int] -> [Board] -> (Int, Board)) -> [Int] -> [Board] -> Int
+solve :: PlayBingo -> [Int] -> [Board] -> Int
 solve playBingo nums boards = lastNumber * sumOfUnmarked
   where
     (lastNumber, bingoBoard) = playBingo 0 nums boards
-    sumOfUnmarked = sum . map num . filter (not . marked) $ bingoBoard
+    sumOfUnmarked = sum . map num . filter (not . marked) . cells $ bingoBoard
 
 solve1 :: [Int] -> [Board] -> Int
 solve1 = solve playBingoUntilFirstWin
@@ -97,6 +89,7 @@ solve1 = solve playBingoUntilFirstWin
 solve2 :: [Int] -> [Board] -> Int
 solve2 = solve playBingoUntilLastWin
 
+main :: IO ()
 main = do
   (nums, boards) <- parseInput <$> readFile "input.txt"
   let part1 =  solve1 nums boards
